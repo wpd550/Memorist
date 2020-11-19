aa1eec1efd7c94f858ab8c9d893f55bf4c8029c7
IKAGAI <136811566@qq.com>
2020-11-20 01:18:49 +0800

    init

---
+++ Memorist/Memorist/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Memorist
+//
+//  Created by dong on 2020/11/20.
+//  Copyright © 2020 Ando. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
